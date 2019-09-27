b9bff0c2f37e7266e973a0ae5e754da0e542c62a
CedricXing <cedricxing0513@gmail.com>
2019-09-27 10:52:35 +0800

    new runsimulation with T=2 and 6 circles

--- runsimulation.py
+++ runsimulation.py
@@ -91,15 +91,17 @@ class SimRunner:
         self.current_time += 10
         temp_state = []
         # classify_state = []
-        waypoint_num = 3
+        waypoint_num = 5
         T = 2
-        for i in range(0,4):
+        for i in range(0,2):
             current_location = self.vehicle.location.global_frame
             if i % 2 == 0:
                 target_delta = [random.uniform(0.0002,0.0003)/waypoint_num,random.uniform(0.0002,0.0003)/waypoint_num,random.uniform(20,30)/waypoint_num]
             else:
                 target_delta = [random.uniform(0.0002,0.0003)/waypoint_num,random.uniform(-0.0003,-0.0002)/waypoint_num,random.uniform(-30,-20)/waypoint_num]
             for j in range(1,waypoint_num+1):
+                if i == 1 and j == 2:
+                    break
                 profile = LocationGlobal(current_location.lat+target_delta[0]*j,current_location.lon+target_delta[1]*j,current_location.alt+target_delta[2]*j)
                 self.profiles.append([profile.lat,profile.lon,profile.alt])
                 # self.profiles.append([profile.lat,profile.lon,profile.alt-current_location.alt+20])
@@ -108,11 +110,13 @@ class SimRunner:
                 temp_state = []
                 while current_t < T:
                     # print_info(self.vehicle)
+                    # print(current_t)
                     temp_state.append([self.vehicle.location.global_frame.lat,self.vehicle.location.global_frame.lon,self.vehicle.location.global_frame.alt
                     ,self.vehicle.attitude.pitch,self.vehicle.attitude.yaw,self.vehicle.attitude.roll,self.vehicle.velocity[0],self.vehicle.velocity[1],self.vehicle.velocity[2]])
                     time.sleep(0.1)
                     current_t += 0.1
                 self.states.append(temp_state)
+
         self.vehicle.close()
         self.sitl.stop()
 
